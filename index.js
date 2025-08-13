05eff5291f47548cd26f4e2b3a4351f6c8fb07a7
marc <marc.llobera@alcort.es>
2025-08-13 14:18:05 +0200

    Update

--- index.js
+++ index.js
@@ -8,6 +8,9 @@ const port = process.env.PORT || 3000;
 
 app.use(cors());
 
+app.use(express.urlencoded({ extended: false }));
+app.use(express.json());
+
 app.use('/public', express.static(`${process.cwd()}/public`));
 
 app.get('/', function(req, res) {
@@ -17,6 +20,34 @@ app.get('/', function(req, res) {
 // Your first API endpoint
 app.get('/api/hello', function(req, res) {
   res.json({ greeting: 'hello API' });
+});
+
+function esFormatoURL(str) {
+  const regex = /^https?:\/\/www\.[a-z0-9-]+(\.[a-z]{2,})+(\/.*)?$/i;
+  return regex.test(str);
+}
+
+let saved_urls = {}
+let num = 0;
+
+app.post('/api/shorturl', function(req, res) {
+  let url = req.body.url
+  if (!url){
+    return res.json({ error: 'invalid url' });
+  }
+
+  if (!esFormatoURL(url)){
+    for (let clave in saved_urls) {
+      if(clave.toString() == url.toString()){
+        return res.redirect(saved_urls[clave]);
+      }
+    }
+    return res.json({ error: 'invalid url' });
+  }
+
+  num = num + 1;
+  saved_urls[num] = url.toString();
+  return res.json({ original_url : url.toString(), short_url : num });
 });
 
 app.listen(port, function() {
